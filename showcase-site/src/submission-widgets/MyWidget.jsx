05150f229aafa8fe2dd717ea1328db5acf8fdb10
heli3939 <luo325692@gmail.com>
2025-03-29 13:15:54 +1100

    Update MyWidget.jsx

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -6,21 +6,20 @@ const MyWidget = () => {
 
   return (
     <div className="p-6 max-w-4xl mx-auto bg-white rounded-xl shadow-lg flex justify-between items-start">
-      <div className="bg-white rounded-xl shadow-md  h-[500px] flex flex-col">
+      <div className="bg-white rounded-xl shadow-md p-4 w-1/2 h-[500px] flex flex-col">
         <h2 className="text-3xl font-bold text-gray-800">Hello {userName}!</h2>
         <div className="text-xl font-bold text-blue-600">
           Daily Tasks
-
         </div>
-        <div className="border-4 bg-green-500 w-40 h-70 bg-clip-border p-3">To-do List
+        <div className="border-4 bg-green-500 w-40 h-100 bg-clip-border p-3">To-do List
         </div>
-        <div className="bg-white rounded-xl shadow-md p-4 flex flex-col justify-end">
+        <div className="bg-white rounded-xl shadow-md h-40 flex flex-col justify-end">
           <div className="text-xl font-bold text-indigo-600">Current Status</div>
           <div className="border-4 bg-blue-500 w-40 h-10 bg-clip-border p-3">HP Bar</div>
           <div className="border-4 bg-cyan-500 w-40 h-10 bg-clip-border p-3">XP Bar</div>
         </div>
       </div>
-      <div className="bg-white rounded-xl shadow-md p-4 w-1/2 h-[500px] flex flex-col justify-between ">
+      <div className="bg-white rounded-xl shadow-md p-4 w-1/2 h-[500px] flex flex-col">
         <div className="text-xl font-bold text-indigo-600">User Profile</div>
         <div className="border-4 bg-blue-500 w-40 h-40 bg-clip-border p-3">Emoji Here</div>
         <div className="border-4 bg-cyan-500 w-50 h-40 bg-clip-border p-3">Level Description</div>
