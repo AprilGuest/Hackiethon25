236b2897b2067e62f73f0450bfdd946d0648b685
AprilGuest <aprilrg9@gmail.com>
2025-03-29 13:06:16 +1100

    Added Habit Component UI

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import { TiPencil, TiTrash } from "react-icons/ti";
 
 const MyWidget = () => {
-  const [habits, setHabits] = useState(["Test Habit"])
+  const [habits, setHabits] = useState([{name: "Test Habit", checked:false}])
 
-  //Tracks the habit that is currently being edited (null if none are being edited)
-  const [editingHabit, setEditingHabit] = useState(null)
+  //Tracks the index of habit that is currently being edited (null if none are being edited)
+  const [editingHabitIndex, setEditingHabitIndex] = useState(null)
 
-  const addHabit = (newHabitName) => setHabits([...habits, newHabitName])
+  const addHabit = (newHabitName) => setHabits([...habits, {name: newHabitName, checked:false}])
 
   const editHabit = (habitIndex, newHabitName) => {
     let newHabits = [...habits]
-    newHabits[habitIndex] = newHabitName
+    newHabits[habitIndex] = {name: newHabitName, checked: habits[habitIndex].checked}
     setHabits(newHabits)
   }
 
@@ -25,15 +26,24 @@ const MyWidget = () => {
     setHabits(newHabits)
   }
 
+  const checkHabit = (habitIndex) => {
+    let newHabits = [...habits]
+    newHabits[habitIndex].checked = !newHabits[habitIndex].checked
+    setHabits(newHabits)
+  }
+
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg">
+    <div className="p-6 mx-auto bg-white rounded-xl w-100 shadow-lg">
       <div className="text-center space-y-4">
         <h2 className="text-xl font-bold text-gray-800">Habit Tracker</h2>
 
         <div className="justify-center bg-white">
           <div>
           <ul>
-            {habits.map((habit, index) => <li key={index}>{habit}</li>)}
+            {/* {habits.map((habit, index) => 
+            <Habit key={index} beingEdited={editingHabitIndex === index} habitName={habit.name} checked={habit.checked}
+            onCheckClicked={() => checkHabit(index)}
+            onEditClicked={() => setEditingHabitIndex({index})}/>)}*/ }
           </ul>
           </div>
         </div>
@@ -41,5 +51,28 @@ const MyWidget = () => {
     </div>
   );
 };
+
+const Habit = ({habitName, beingEdited, checked, onEditClicked, onDeleteClicked, onCheckClicked}) => {
+  return (
+    <div className="flex justify-between gap-5">
+      <input type="checkbox" className='border-gray-400 hover:scale-140 hover:border-black transition-all'
+      onClick={onCheckClicked}/>
+      
+      {/* Name/edit box */}
+      {!beingEdited ? 
+      <p>{habitName}</p> : 
+      <input type="text" className='border-1 rounded-md'></input>
+      }
+
+      { /* Edit & Delete Icons */ }
+
+      <button className='border-white border-1 rounded-md hover:border-black hover:scale-125 ml-auto box-border
+                        transition-all'
+                        onClick={onEditClicked}><TiPencil /></button>
+      <button><TiTrash className='border-white border-1 rounded-md scale-120 hover:border-black hover:scale-160 ml-auto box-border
+                        transition-all'/></button>
+    </div>
+  )
+}
 
 export default MyWidget;
